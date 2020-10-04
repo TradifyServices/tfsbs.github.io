49d2ecd174d3db2a8f5dcf27d44a1d9f068d1aa4
shaz <shaz.e.j@gmail.com>
2020-10-04 14:23:48 +0300

    added pwa files

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('push', function(event) {
+    event.waitUntil(
+      self.registration.showNotification('Got Push?', {
+        body: 'Push Message received'
+     }));
+  });
+/** An empty service worker! */
+self.addEventListener('fetch', function(event) {
+  /** An empty fetch handler! */
+});
