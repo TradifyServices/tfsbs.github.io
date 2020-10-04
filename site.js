d3bc58358ad00bb85df650f4cd0bb1eaf0f44652
shaz <shaz.e.j@gmail.com>
2020-10-04 17:25:59 +0300

    pwa

--- site.js
+++ site.js
@@ -3,7 +3,7 @@ if("serviceWorker" in navigator)
     navigator.serviceWorker.register("sw.js").then(registration=>{
         console.log("SW Registered!");
         console.log(registration);
-    }).catch(console.error => {
+    }).catch(error => {
  console.log("SW Registration Failed");
  console.log(error);
     });
